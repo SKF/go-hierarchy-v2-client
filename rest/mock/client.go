eb87be24735df6da7999928537c9018e490effe0
Johan Dahlberg <johan@dahlis.xyz>
2022-02-09 15:14:14 +0100

    fix typo in mock return

--- rest/mock/client.go
+++ rest/mock/client.go
@@ -72,7 +72,7 @@ func (c *HierarchyClientMock) GetSubtreeCount(ctx context.Context, id uuid.UUID,
 
 func (c *HierarchyClientMock) GetSubtreePage(ctx context.Context, id uuid.UUID, filter rest.TreeFilter, continuationToken string) (models.GetSubtreeResponse, error) {
 	args := c.Called(ctx, id, filter)
-	return args.Get(0).([]models.Node), args.Error(1)
+	return args.Get(0).(models.GetSubtreeResponse), args.Error(1)
 }
 
 func (c *HierarchyClientMock) LockNode(ctx context.Context, id uuid.UUID, recursive bool) error {
